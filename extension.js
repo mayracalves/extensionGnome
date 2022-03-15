cff28255ac19f5a0b5fa6e9b2f9bcdc640318404
Mayra Alves <mayracalves@pop-os.localdomain>
2022-03-15 19:58:29 -0300

    chore: substitui texto por um contador

--- extension.js
+++ extension.js
@@ -1,14 +1,22 @@
-// const Clutter = import.gt.Clutter;
+// const Clutter = imports.gi.Clutter;
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
 
-let panelButton, panelButtonText;
+let panelButton, panelButtonText, timeout;
+let counter = 0;
+
+function setButtonText () {
+    counter++;
+    panelButtonText.set_text( counter.toString() )
+    return true;
+}
 
 function init () {
     panelButton = new St.Bin({
         style_class : "panel-button",
     });
-    let panelButtonText = new St.Label({
+    panelButtonText = new St.Label({
         style_class : "exemplePanelText",
         text : "Mayrinha detonando",
         // y_align: Clutter.ActorAlign.CENTER,
@@ -18,8 +26,10 @@ function init () {
 
 function enable () {
     Main.panel._rightBox.insert_child_at_index(panelButton, 1);
+    timeout = Mainloop.timeout_add_seconds(1.0, setButtonText);
 }
 
 function disable () {
+    Mainloop.source_remove(timeout);
     Main.panel._rightBox.remove_child(panelButton);
 }
